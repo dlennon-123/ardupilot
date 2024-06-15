6b4d49c3f90fefda7f0196639eddf92cfac2d6a8
Dlennon <pampr.development@gmail.com>
2024-06-14 18:45:37 -0700

    Update heading loiter to use throttle and lateral movement

--- Rover/mode_heading_loiter.cpp
+++ Rover/mode_heading_loiter.cpp
@@ -20,26 +20,18 @@ bool ModeHeadingLoiter::_enter()
 
 void ModeHeadingLoiter::update()
 {
-    // get distance (in meters) to destination
-    _distance_to_destination = rover.current_loc.get_distance(_destination);
+    Vector2f _distance_to_destination_loiter = rover.current_loc.get_distance_NE(_destination);
 
     const float loiter_radius = g2.loit_radius;
-
-    // if within loiter radius slew desired speed towards zero and use existing desired heading
-    if (_distance_to_destination <= loiter_radius) {
-        const float desired_speed_within_radius = 0.0f;
-        _desired_speed = attitude_control.get_desired_speed_accel_limited(desired_speed_within_radius, rover.G_Dt);
-    } else {
-        // P controller with hard-coded gain to convert distance to desired speed
-        _desired_speed = MIN((_distance_to_destination - loiter_radius) * g2.loiter_speed_gain, g2.wp_nav.get_default_speed());
-    }
-
     // 0 turn rate is no limit
     float turn_rate = 0.0;
+    float _desired_throttle = (_distance_to_destination_loiter.y - loiter_radius) * g2.loiter_speed_gain;
+    float _desired_lateral = (_distance_to_destination_loiter.x - loiter_radius) * g2.loiter_speed_gain;
 
     // run steering and throttle controllers
     calc_steering_to_heading(_desired_yaw_cd, turn_rate);
-    calc_throttle(_desired_speed, true);
+    calc_throttle(_desired_throttle, true);
+    g2.motors.set_lateral(_desired_lateral);
 }
 
 // get desired location
